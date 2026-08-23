0af4779c8b9c8c16d390695a8eb693f6026c6b9d
kongweha <kongweha.i@chula.ac.th>
2026-08-23 14:59:51 +0700

    Deploy config/firebase-config.js - Chula AIX Custom Registration & Firebase Analytics

---
+++ config/firebase-config.js
@@ -0,0 +1,14 @@
+// ═══════════════════════════════════════════════════════════════
+// CHULA AIX - FIREBASE CONFIGURATION
+// ═══════════════════════════════════════════════════════════════
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCLQkUz59m3twJMQFoBL5xO9_VmZZHwU0c",
+  authDomain: "chula-aix.firebaseapp.com",
+  databaseURL: "https://chula-aix-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chula-aix",
+  storageBucket: "chula-aix.firebasestorage.app",
+  messagingSenderId: "692837434850",
+  appId: "1:692837434850:web:44867ec5fa00b01d5525bb",
+  measurementId: "G-TP02CWZWY2"
+};
